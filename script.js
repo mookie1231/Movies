8435359be96164b390ed009ca5f34ffe492e5c13
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-02-29 09:22:49 -0500

    complete front end

--- script.js
+++ script.js
@@ -11,15 +11,43 @@ function returnMovies(url) {
         console.log(data.results);
         data.results.forEach(element => {
             const div_card = document.createElement('div');
+            div_card.setAttribute('class','card')
             const div_row = document.createElement('div');
+            div_row.setAttribute('class','row')
             const div_column = document.createElement('div');
+            div_column.setAttribute('class','column')
             const image = document.createElement('img');
+            image.setAttribute('class','image')
+            image.setAttribute('id','image')
             const title = document.createElement('h3');
+            title.setAttribute('class','h3')
             const center = document.createElement('center');
 
             title.innerHTML = `${element.title}`;
             image.src = IMG_PATH + element.poster_path;
+
+            center.appendChild(image);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+
+            main.appendChild(div_row);
+
+
             
         })
     })
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = ''
+
+    const searchItem = search.ariaValueMax;
+
+    if(searchItem) {
+        returnMovies(SEARCHAPI + searchItem)
+        search.value = ''
+    }
+})
